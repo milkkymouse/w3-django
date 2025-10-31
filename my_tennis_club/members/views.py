723fb559245554dc56ea858b7c1153e723199f98
milk <mikhaelle.duarte@education.nsw.gov.au>
2025-10-31 01:21:30 +0000

    Added Master Template

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -9,3 +9,11 @@ def members(request):
         'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Members.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember' : mymember,
+    }
+    return HttpReponse(template.render(context, request))
--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m$^=!szhu2nn8uii%-1*&6k21&q%f^@^b))qho7cx&2-_w)$5j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
